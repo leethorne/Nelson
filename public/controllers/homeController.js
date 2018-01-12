99fe4412844592d4db0946fa1d7606234752ca15
Felicia Thorne <feliciathorne@Felicias-MBP.attlocal.net>
2018-01-12 13:42:21 -0800

    add report function is working! data bound on report page

--- public/controllers/homeController.js
+++ public/controllers/homeController.js
@@ -2,10 +2,23 @@ app.controller("homeController", function ($scope, $state, $stateParams, homeSer
 
     homeService.getTeachers()
     .then(function(response) {
-        console.log(response)
+        console.log(response.data.teacherList)
+        $scope.teachers = response.data.teacherList
     }, function(error) {
         console.log(error)
         alert("Sorry! Something went wrong...")
     })
+
+
+    $scope.addReport = function () {
+        console.log("in add report")
+        homeService.addReport($scope.report)
+            .then(function (response) {
+                console.log("RES report: ", response)
+            }, function (error) {
+                console.log(error)
+                alert("Sorry! Something went wrong...")
+            })
+    }
 
 })
--- public/services/homeService.js
+++ public/services/homeService.js
@@ -4,8 +4,9 @@ app.service("homeService", function ($http, $state) {
         return $http.get('http://localhost:3000/api/v1/home/teacher')
     }
 
-    this.addReport = function() {
-        return $http.post('http://localhost:3000/api/v1/home/')
+    this.addReport = function(report) {
+        console.log(report)
+        return $http.post('http://localhost:3000/api/v1/home/', report)
     }
 
 })
